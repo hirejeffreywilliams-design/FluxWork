51115dcf0783dc03188b28751ee973b2fb291592
hirejeffreywilliams-design <hirejeffreywilliams@gmail.com>
2026-04-18 11:14:46 +0000

    chore: export domain + server modules from index

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,2 @@
-/**
- * FluxWork
- * Reserved future-feature module within the Omnivex Ecosystem.
- */
-
-export const REPO_NAME = 'FluxWork';
-
-export function healthcheck() {
-  return { ok: true, repo: REPO_NAME } as const;
-}
+export * from './domain/workItem';
+export * from './server/healthRoute';
